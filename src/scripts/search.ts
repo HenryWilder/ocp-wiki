26359ffc3c8181816f3fa19dbd7bc447aff16abe
Henry Wilder <henrythepony@gmail.com>
2023-03-26 12:37:01 -0400

    Got script working - still not functional

--- src/scripts/search.ts
+++ src/scripts/search.ts
@@ -9,18 +9,20 @@ const index: SearchIndex[] = indexFile;
 
 const flatten = <Type>(accum: Type[], curr: Type[]) => accum.concat(curr)
 
-export const queryPages = (query: string): string[] => {
+const queryPages = (query: string): string[] => {
     // List of words in query
     const words: string[] = query.split(" ");
-    const results: string[] = [
-        ...new Set(
-            words
-                .map((word: string) => index.filter(element => (element.keyword == word) || element.aliases.includes(word)))
-                .reduce(flatten)
-                .map((item: SearchIndex) => item.pages)
-                .reduce(flatten)
-                .sort((a: string, b: string) => a.localeCompare(b))
-        )
-    ];
+    const rawResults: SearchIndex[][] = words.map((word: string) => index.filter(element => (element.keyword == word) || element.aliases.includes(word)));
+    if (rawResults.length === 0)
+        return [];
+    const rawResultsConcat: SearchIndex[] = rawResults.reduce(flatten);
+    const resultPageLists: string[][] = rawResultsConcat.map((item: SearchIndex) => item.pages);
+    if (resultPageLists.length === 0)
+        return [];
+    const resultPages: string[] = resultPageLists.reduce(flatten);
+    const resultsUnique: string[] = [...new Set(resultPages)];
+    const results: string[] = resultsUnique.sort((a: string, b: string) => a.localeCompare(b));
     return results;
 }
+
+export default queryPages;
