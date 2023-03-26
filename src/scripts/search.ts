61388424b393d16a8166105fd64c056f506196da
Henry Wilder <henrythepony@gmail.com>
2023-03-26 12:44:56 -0400

    Added search functionality

--- src/scripts/search.ts
+++ src/scripts/search.ts
@@ -1,7 +1,11 @@
+export interface PageLink {
+    name: string;
+    path: string;
+}
 interface SearchIndex {
     keyword: string;
     aliases: string[];
-    pages: string[];
+    pages: PageLink[];
 }
 
 import indexFile from "../../public/data/search-index.json";
@@ -9,20 +13,18 @@ const index: SearchIndex[] = indexFile;
 
 const flatten = <Type>(accum: Type[], curr: Type[]) => accum.concat(curr)
 
-const queryPages = (query: string): string[] => {
+export const queryPages = (query: string): PageLink[] => {
     // List of words in query
     const words: string[] = query.split(" ");
     const rawResults: SearchIndex[][] = words.map((word: string) => index.filter(element => (element.keyword == word) || element.aliases.includes(word)));
     if (rawResults.length === 0)
         return [];
     const rawResultsConcat: SearchIndex[] = rawResults.reduce(flatten);
-    const resultPageLists: string[][] = rawResultsConcat.map((item: SearchIndex) => item.pages);
+    const resultPageLists: PageLink[][] = rawResultsConcat.map((item: SearchIndex) => item.pages);
     if (resultPageLists.length === 0)
         return [];
-    const resultPages: string[] = resultPageLists.reduce(flatten);
-    const resultsUnique: string[] = [...new Set(resultPages)];
-    const results: string[] = resultsUnique.sort((a: string, b: string) => a.localeCompare(b));
+    const resultPages: PageLink[] = resultPageLists.reduce(flatten);
+    const resultsUnique: PageLink[] = [...new Set(resultPages)];
+    const results: PageLink[] = resultsUnique.sort((a: PageLink, b: PageLink) => a.name.localeCompare(b.name));
     return results;
 }
-
-export default queryPages;
