c335a90dcf77a5f3de8a2eb028a4010c930bd6d7
Henry Wilder <henrythepony@gmail.com>
2023-04-07 23:07:35 -0400

    Significant refactor adding dynamic paths and markdown

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx'
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'http://ocp-wiki.net'
+    site: 'http://ocp-wiki.net',
+    output: 'static',
+    integrations: [mdx()]
 });
--- src/content/config.ts
+++ src/content/config.ts
@@ -1,6 +1,27 @@
 import { z, defineCollection } from 'astro:content';
 
+const objectCollection = defineCollection({
+  schema: z.object({
+    name: z.string(),
+    author: z.string().default('Uniconwizard'), // Uni made like 90% of the content, so he's the default author
+    version: z.number(),
+    title: z.string(),
+    category: z.string(),
+    tags: z.array(z.string()).optional(),
+    image: z.string().optional(),
+    draft: z.boolean().default(false),
+  }),
+});
+
+const authorCollection = defineCollection({
+  schema: z.object({
+    name: z.string(),
+    title: z.string(),
+    category: z.string(),
+  })
+});
+
 export const collections = {
-    'objects': defineCollection({ /* ... */ }),
-    'authors': defineCollection({ /* ... */ }),
+    'objects': objectCollection,
+    'authors': authorCollection,
   };
